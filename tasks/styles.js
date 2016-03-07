0d5233e7bb823781fb70d69a6d2ed241cbc65e93
Pavel Botsman <pavel-botsman@yandex.ru>
2016-03-07 19:45:54 +0300

    initial commit

---
+++ tasks/styles.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const $ = require('gulp-load-plugins')();
+const cleanCss = require('gulp-clean-css');
+const gulp = require('gulp');
+const combine = require('stream-combiner2').obj;
+
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
+
+module.exports = function(options) {
+  return function() {
+    return combine(
+        gulp.src(options.src),
+        $.if(isDevelopment, $.sourcemaps.init()),
+        $.less(),
+        $.if(!isDevelopment , cleanCss()),
+        $.if(isDevelopment, $.sourcemaps.write()),
+        gulp.dest('public')
+    ).on('error', $.notify.onError());
+  };
+
+};
